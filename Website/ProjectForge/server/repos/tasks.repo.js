2cd1c65515deee44f450dfb1e68f2f37a2cff6b0
Codetheist <56620151+Codetheist@users.noreply.github.com>
2026-05-26 19:27:47 -0400

    Remove local isValidDate function and ensure proper import in TasksRepo

--- Website/ProjectForge/server/repos/tasks.repo.js
+++ Website/ProjectForge/server/repos/tasks.repo.js
@@ -2,6 +2,7 @@
 const { uid } = require("../utils/ids");
 const { createError } = require("../utils/httpError");
 const { dbConnection } = require("../db/db");
+const { isValidDate } = require("../utils/date");
 
 class TasksRepo {
     constructor() {
@@ -202,19 +203,6 @@ class TasksRepo {
         
         return { task, message: "Task deleted successfully" };
     }
-}
-
-function isValidDate(date) {
-    if (date == null) return true;
-    if (!/^[1-2]\d{3}-\d{2}-\d{2}$/.test(date)) return false;
-    
-    const [year, month, day] = date.split("-").map(Number);
-    const dateObj = new Date(year, month - 1, day);
-    return (
-        dateObj.getFullYear() === year &&
-        dateObj.getMonth() === month - 1 &&
-        dateObj.getDate() === day
-    );
 }
 
 module.exports = {
