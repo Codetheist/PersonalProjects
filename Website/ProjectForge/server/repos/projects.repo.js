c2a2a8d38c7895b57495e631c65540a66efcb87b
Codetheist <56620151+Codetheist@users.noreply.github.com>
2026-05-26 19:27:37 -0400

    Remove duplicate isValidDate function and ensure proper import in ProjectsRepo

--- Website/ProjectForge/server/repos/projects.repo.js
+++ Website/ProjectForge/server/repos/projects.repo.js
@@ -2,6 +2,7 @@
 const { uid } = require("../utils/ids");
 const { createError } = require("../utils/httpError");
 const { dbConnection } = require("../db/db");
+const { isValidDate } = require("../utils/date");
 
 class ProjectsRepo {
     constructor() {
@@ -171,19 +172,6 @@ class ProjectsRepo {
         
         return { project, message: "Project deleted successfully" };
     }
-}
-
-function isValidDate(date) {
-    if (date == null) return true;
-    if (!/^[1-2]\d{3}-\d{2}-\d{2}$/.test(date)) return false;
-    
-    const [year, month, day] = date.split("-").map(Number);
-    const dateObj = new Date(year, month - 1, day);
-    return (
-        dateObj.getFullYear() === year &&
-        dateObj.getMonth() === month - 1 &&
-        dateObj.getDate() === day
-    );
 }
 
 module.exports = {
