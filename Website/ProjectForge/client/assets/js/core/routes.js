df4efb67d6ca89cc0022f82c6a1bd599009e0fe3
Codetheist <56620151+Codetheist@users.noreply.github.com>
2026-06-13 14:42:53 -0400

    Add routes.js for defining application routes and importing necessary modules

---
+++ Website/ProjectForge/client/assets/js/core/routes.js
@@ -0,0 +1,11 @@
+// Routes
+import { elements } from '../shared/dom.js';
+import { showAuthState, showLandingState } from '../shared/ui.js';
+
+// Define application routes
+export const PAGE_ROUTES = {
+    home: '/',
+    dashboard: '/dashboard',
+    projects: '/project',
+}
+
